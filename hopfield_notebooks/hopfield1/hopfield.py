4d1147aec3338f4c288a0e8feab11be3cf15eb5e
catubc <mitelutco@gmail.com>
2022-07-12 17:26:15 +0200

    updated hopfield energy srufcae computation

--- hopfield_notebooks/hopfield1/hopfield.py
+++ hopfield_notebooks/hopfield1/hopfield.py
@@ -67,7 +67,79 @@ class hopfield:
             elif new_state > 0:
                 state[idx] = 1
         return state
-    
+
+
+    #
+    def predict_no_plot(self,
+						mat_input,
+						iteration,
+	                    asyn=False,
+                        async_iteration=200):
+        #
+        input_shape = mat_input.shape
+        mat_input = np.where(mat_input < 0.5,-1,1)
+        e_list = []
+
+        e = self.energy(mat_input.flatten())
+        e_list.append(e)
+        state = mat_input.flatten()
+        
+        #
+        if asyn:
+            states =[]
+            print('Starting asynchronous update with ',iteration,' iterations')
+            for i in range(iteration):
+                idxes = np.random.choice(np.arange(state.size), 
+                                          state.size, replace=False)
+				# change the async update to prperly loop over all units
+                #for j in range(async_iteration):
+                    #idx = np.random.randint(state.size)
+                for idx in tqdm(idxes):   
+                    state = self.update(state,idx)
+                    state_show = np.where(state < 1,0,1).reshape(input_shape)
+
+                new_e = self.energy(state) #-0.5*np.matmul(np.matmul(state.T,self.W),state)
+                print('Iteration#',i,', Energy: ',new_e)
+                if new_e == e:
+                    print('Energy remain unchanged, update will now stop.')
+                    break
+                e = new_e
+                e_list.append(e)
+        #
+        else:
+            print('Starting synchronous update with ',iteration,' iterations')
+            states = []
+            states.append(state.copy())
+            for i in range(iteration):
+                state = self.update(state)
+                state = np.where(state < 1, 0,1)
+                states.append(state.copy())
+                #state_show = state.copy().reshape(input_shape)
+                #graph.set_data(state_show*255)
+                #axs[0].set_title('Sync update Iteration #%i' %i)
+                #fig.canvas.draw_idle()
+                #plt.pause(0.5)
+                new_e = self.energy(state)
+                #new_e = -0.5*np.matmul(np.matmul(state.T,self.W),state)
+                print('Iteration#',i,', Energy: ',new_e)
+                if new_e == e:
+                    print('Energy remain unchanged, update will now stop.')
+                    break
+                e = new_e
+                e_list.append(e)
+        print('Iteration completed, update will now stop.')
+        
+        # show residual
+        #im2 = axs[2].imshow(state_show-original_img, cmap='binary')
+        #axs[2].set_title("residual difference")
+        #im = ax.imshow(data, cmap='binary')
+        #fig.colorbar(im2)
+        
+        #plt.pause(2)
+        #plt.close()
+        return np.where(state < 1,0,1).reshape(input_shape),e_list, states
+        
+        
     #
     def predict(self,mat_input,
                      original_img,
